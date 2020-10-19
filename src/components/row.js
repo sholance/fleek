3047ffabdca9b636ef79f2c49cdd4771b323f0bd
Sholance <ohksamie@gmail.com>
2020-10-20 00:18:04 +0100

    added banner and finished row

--- src/components/row.js
+++ src/components/row.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
+import "./row.css";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -13,17 +14,20 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
-
-  const base_url = `https://image.tmbd.org/t/p/original`;
+  const base_url = `https://image.tmdb.org/t/p/original/`;
 
   return (
     <div className="row">
       <h2>{title}</h2>
 
-      <div className="row_cardboard">
+      <div className="row__cardboards">
         {movies.map((movie) => (
-          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          <img
+            key={movie.id}
+            className={`row__cardboard ${isLargeRow && "row__cardboardLarge"}`}
+            src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
